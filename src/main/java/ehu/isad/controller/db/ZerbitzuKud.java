654c8c064fc4256034997492794d841d53cdb6a4
jonander <jonanderasua@gmail.com>
2020-10-23 09:38:20 +0200

    Ez du aurkitzen title zutabea + publishers sartzean errorea ematen du

--- src/main/java/ehu/isad/controller/db/ZerbitzuKud.java
+++ src/main/java/ehu/isad/controller/db/ZerbitzuKud.java
@@ -79,7 +79,7 @@ public class ZerbitzuKud {
         int numberOfPages = d.getPages();
         String publisher = d.getArgitaretxea();
        // unekoEskaera = "insert into openlibrary.details values("+numberOfPages+",'"+isbn+"',`'"+publisher+"'`)";
-        unekoEskaera="INSERT INTO `openlibrary`.`details` (`numberOfPages`, `isbn`, `publisher`) VALUES ("+numberOfPages+",'"+isbn+"',`'"+publisher+"'`)";
+        unekoEskaera="INSERT INTO `openlibrary`.`details` (`numberOfPages`, `isbn`, `publisher`) VALUES ("+numberOfPages+",'"+isbn+"',`'"+publisher+"'`)"; //publishers sartzean errorea ematen du
         System.out.println(unekoEskaera);
         dbk.execSQL(unekoEskaera);
 
